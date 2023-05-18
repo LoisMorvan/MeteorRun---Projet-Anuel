49a8de662999795619fa0b05870937204d68b0c4
MORVAN Loïs <lmorvan6@myges.fr>
2023-05-18 14:16:32 +0200

    Ajout de la connexion à la base de données et la possibilité de l'exporter

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import MainMenu from "./MainMenu.js";
 import MainGame from "./MainGame.js";
 import GameOver from "./GameOver.js";
 
+const sqlite3 = require('sqlite3').verbose();
+
 export const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -17,3 +19,6 @@ export const config = {
 };
 
 let game = new Phaser.Game(config);
+
+// Ouvrir une connexion à la base de données SQLite
+export const db = new sqlite3.Database('./database.sqlite');
